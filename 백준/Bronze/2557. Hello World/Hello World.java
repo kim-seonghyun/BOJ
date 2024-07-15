2cde7d0e6b279e9815147c9018f3d72aa3e4ca85
김성현 <60726663+kim-seonghyun@users.noreply.github.com>
2024-07-15 21:26:18 +0900

    [Bronze V] Title: Hello World, Time: 96 ms, Memory: 13928 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class Main {
+
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		System.out.println("Hello World!");
+		
+	}	
+}
