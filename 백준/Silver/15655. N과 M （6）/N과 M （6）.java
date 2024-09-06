ee2e541c64b33cd5b8303b3a25e07931435eb511
김성현 <60726663+kim-seonghyun@users.noreply.github.com>
2024-09-06 17:32:37 +0900

    [Silver III] Title: N과 M (6), Time: 100 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * 1. 문제 설명:
+ * 오름차순 조합
+ */
+public class Main {
+    static BufferedReader br;
+    static StringTokenizer st;
+    static int N;
+    static int M;
+    static int[] board;
+
+    static int[] tmp;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        inputValue();
+        Arrays.sort(board);
+        dfs(0, 0);
+        System.out.println(sb);
+    }
+
+    private static void dfs(int depth, int current) {
+        if (depth == M) {
+            for (int i = 0; i < tmp.length; i++) {
+                sb.append(tmp[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = current; i < N; i++) {
+            tmp[depth] = board[i];
+            dfs(depth + 1, i + 1);
+        }
+    }
+
+    private static void inputValue() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        board = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            board[i] = Integer.parseInt(st.nextToken());
+        }
+        tmp = new int[M];
+    }
+}
