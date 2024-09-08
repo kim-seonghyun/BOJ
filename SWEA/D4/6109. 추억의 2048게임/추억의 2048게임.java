a0ef9f95b7bcb7c2c1b67210432725f0dcfc10e6
김성현 <60726663+kim-seonghyun@users.noreply.github.com>
2024-09-08 18:33:22 +0900

    [D4] Title: 추억의 2048게임, Time: 260 ms, Memory: 39,632 KB -BaekjoonHub

--- SWEA/D4/6109. 추억의 2048게임/추억의 2048게임.java
+++ SWEA/D4/6109. 추억의 2048게임/추억의 2048게임.java
@@ -1,250 +1,59 @@
 import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
+/**
+ * @author 김성현
+ */
 public class Solution {
     static int N;
-    static String dir;
     static BufferedReader br;
     static StringTokenizer st;
     static int[][] board;
+    static int maxValue = Integer.MIN_VALUE;
+    static String direction;
 
-    public static void main(String[] args) throws NumberFormatException, IOException {
-
-        // br = new BufferedReader(new FileReader("input.txt"));
+    public static void main(String[] args) throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-
-        for (int tc = 1; tc <= T; tc++) {
-
-            inputValue();
+        int t = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= t; tc++) {
             System.out.printf("#%d\n", tc);
-            switch (dir) {
+            inputValue();
+
+            switch (direction){
                 case "up":
                     up();
                     break;
                 case "down":
                     down();
                     break;
-
-                case "right":
-                    right();
-                    break;
-
                 case "left":
                     left();
                     break;
-
-            }
-
-        }
-
-    }
-
-    private static void up() {
-        LinkedList<LinkedList<Integer>> a = new LinkedList<>();
-        for (int i = 0; i < N; i++) {
-            a.add(new LinkedList<>());
-            for (int j = 0; j < N; j++) {
-                if (board[j][i] != 0) {
-                    a.get(i).add(board[j][i]);
-                }
-            }
-        }
-        LinkedList<LinkedList<Integer>> b = new LinkedList<>();
-        // 블록 합치는 코드
-        for (int linkIndex = 0; linkIndex < N; linkIndex++) {
-            LinkedList<Integer> tmp = new LinkedList<>();
-
-            for (int i = 0; i < a.get(linkIndex).size(); i++) {
-                if (i == a.get(linkIndex).size() - 1) {
-                    tmp.add(a.get(linkIndex).get(i));
+                case "right":
+                    right();
                     break;
+            }
+
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    System.out.print(board[i][j] + " ");
                 }
-                if (a.get(linkIndex).get(i).intValue() == a.get(linkIndex).get(i + 1).intValue()) {
-                    tmp.add(a.get(linkIndex).get(i) * 2);
-                    i++;
-                } else {
-                    tmp.add(a.get(linkIndex).get(i).intValue());
-                    if (i == (a.get(linkIndex).size() - 2)) {
-                        tmp.add(a.get(linkIndex).get(i + 1));
-                        break;
-                    }
-                }
-
-            }
-            b.add(tmp);
-
-        }
-        int[][] resultArr = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < b.get(i).size(); j++) {
-                resultArr[j][i] = b.get(i).get(j);
+                System.out.println();
             }
         }
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                System.out.print(resultArr[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
 
-    private static void down() {
-        LinkedList<LinkedList<Integer>> a = new LinkedList<>();
-        for (int i = 0; i < N; i++) {
-            a.add(new LinkedList<>());
-            for (int j = N - 1; j >= 0; j--) {
-                if (board[j][i] != 0) {
-                    a.get(i).add(board[j][i]);
-                }
-            }
 
-        }
-        LinkedList<LinkedList<Integer>> b = new LinkedList<>();
-        for (int linkIndex = 0; linkIndex < N; linkIndex++) {
-            LinkedList<Integer> tmp = new LinkedList<>();
-
-            for (int i = 0; i < a.get(linkIndex).size(); i++) {
-                if (i == a.get(linkIndex).size() - 1) {
-                    tmp.add(a.get(linkIndex).get(i).intValue());
-                    break;
-                }
-                if (a.get(linkIndex).get(i).intValue() == a.get(linkIndex).get(i + 1).intValue()) {
-                    tmp.add(a.get(linkIndex).get(i) * 2);
-                    i++;
-                } else {
-                    tmp.add(a.get(linkIndex).get(i));
-                    if (i == (a.get(linkIndex).size() - 2)) {
-                        tmp.add(a.get(linkIndex).get(i + 1));
-                        break;
-                    }
-                }
-            }
-            b.add(tmp);
-        }
-        int[][] resultArr = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < b.get(i).size(); j++) {
-                resultArr[N - 1 - j][i] = b.get(i).get(j);
-            }
-        }
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                System.out.print(resultArr[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-
-    private static void left() {
-        LinkedList<LinkedList<Integer>> a = new LinkedList<>();
-        for (int i = 0; i < N; i++) {
-            a.add(new LinkedList<>());
-            for (int j = 0; j < N; j++) {
-                if (board[i][j] != 0) {
-                    a.get(i).add(board[i][j]);
-                }
-            }
-        }
-        LinkedList<LinkedList<Integer>> b = new LinkedList<>();
-        for (int linkIndex = 0; linkIndex < N; linkIndex++) {
-            LinkedList<Integer> tmp = new LinkedList<>();
-            // 같으면 합치고
-            // 다르면 그냥 두기
-
-            for (int i = 0; i < a.get(linkIndex).size(); i++) {
-
-                if (i == a.get(linkIndex).size() - 1) {
-                    tmp.add(a.get(linkIndex).get(i));
-                    break;
-                }
-                if (a.get(linkIndex).get(i).intValue() == a.get(linkIndex).get(i + 1).intValue()) {
-                    tmp.add(a.get(linkIndex).get(i) * 2);
-                    i++;
-                } else {
-                    tmp.add(a.get(linkIndex).get(i));
-                    if (i == (a.get(linkIndex).size() - 2)) {
-                        tmp.add(a.get(linkIndex).get(i + 1));
-                        break;
-                    }
-                }
-
-            }
-            b.add(tmp);
-
-        }
-        int[][] resultArr = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < b.get(i).size(); j++) {
-                resultArr[i][j] = b.get(i).get(j);
-            }
-        }
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                System.out.print(resultArr[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-
-    private static void right() {
-        LinkedList<LinkedList<Integer>> a = new LinkedList<>();
-        for (int i = 0; i < N; i++) {
-            a.add(new LinkedList<>());
-            for (int j = N - 1; j >= 0; j--) {
-                if (board[i][j] != 0) {
-                    a.get(i).add(board[i][j]);
-
-                }
-            }
-
-        }
-        LinkedList<LinkedList<Integer>> b = new LinkedList<>();
-        for (int linkIndex = 0; linkIndex < N; linkIndex++) {
-            LinkedList<Integer> tmp = new LinkedList<>();
-
-            for (int i = 0; i < a.get(linkIndex).size(); i++) {
-                if (i == a.get(linkIndex).size() - 1) {
-                    tmp.add(a.get(linkIndex).get(i));
-                    break;
-                }
-                if (a.get(linkIndex).get(i).intValue() == a.get(linkIndex).get(i + 1).intValue()) {
-                    tmp.add(a.get(linkIndex).get(i) * 2);
-                    i++;
-                } else {
-                    tmp.add(a.get(linkIndex).get(i));
-                    if (i == (a.get(linkIndex).size() - 2)) {
-                        tmp.add(a.get(linkIndex).get(i + 1));
-                        break;
-                    }
-                }
-
-            }
-            b.add(tmp);
-
-        }
-        int[][] resultArr = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < b.get(i).size(); j++) {
-                resultArr[i][N - 1 - j] = b.get(i).get(j);
-            }
-        }
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                System.out.print(resultArr[i][j] + " ");
-            }
-            System.out.println();
-        }
     }
 
     private static void inputValue() throws IOException {
         st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
-        dir = st.nextToken();
+        direction = st.nextToken();
         board = new int[N][N];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
@@ -252,7 +61,155 @@ public class Solution {
                 board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+    }
+
+    private static void dfs(int depth) {
+        if (depth == 5) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    maxValue = Math.max(board[i][j], maxValue);
+                }
+            }
+            return;
+        }
+        int[][] tmp = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            System.arraycopy(board[i], 0, tmp[i], 0, N);
+        }
+        for (int dir = 0; dir < 4; dir++) {
+
+            switch (dir) {
+                case 0:
+                    up();
+                    break;
+                case 1:
+                    down();
+                    break;
+                case 2:
+                    left();
+                    break;
+                case 3:
+                    right();
+                    break;
+            }
+            dfs(depth + 1);
+        }
+        for (int i = 0; i < N; i++) {
+            System.arraycopy(tmp[i], 0, board[i], 0, N);
+        }
 
     }
 
+    public static void up() {
+        for (int j = 0; j < N; j++) {
+            Stack<Integer> stack = new Stack<>();
+            for (int i = N - 1; i >= 0; i--) {
+                if (board[i][j] > 0) {
+                    stack.add(board[i][j]);
+                    board[i][j] = 0;
+                }
+            }
+            int index = 0;
+            while (!stack.empty()) {
+                int val1 = stack.pop();
+                if (!stack.empty()) {
+                    int val2 = stack.pop();
+                    if (val1 == val2) {
+                        board[index++][j] = val1 * 2;
+                    } else {
+                        board[index++][j] = val1;
+                        stack.add(val2);
+                    }
+                } else {
+                    board[index++][j] = val1;
+                }
+            }
+        }
+    }
+
+    public static void down() {
+        for (int j = 0; j < N; j++) {
+            Stack<Integer> stack = new Stack<>();
+            for (int i = 0; i < N; i++) {
+                if (board[i][j] > 0) {
+                    stack.add(board[i][j]);
+                    board[i][j] = 0;
+                }
+            }
+            int index = N - 1;
+            while (!stack.empty()) {
+                int val1 = stack.pop();
+                if (!stack.empty()) {
+                    int val2 = stack.pop();
+                    if (val1 == val2) {
+                        board[index--][j] = val1 * 2;
+                    } else {
+                        board[index--][j] = val1;
+                        stack.add(val2);
+                    }
+                } else {
+                    board[index--][j] = val1;
+                }
+            }
+        }
+    }
+
+    public static void left() {
+        for (int i = 0; i < N; i++) {
+            Stack<Integer> stack = new Stack<>();
+            for (int j = N - 1; j >= 0; j--) {
+                if (board[i][j] > 0) {
+                    stack.add(board[i][j]);
+                }
+                board[i][j] = 0;
+            }
+            int index = 0;
+
+            while (!stack.empty()) {
+                int val1 = stack.pop();
+                if (!stack.empty()) {
+                    int val2 = stack.pop();
+                    if (val1 == val2) {
+                        board[i][index++] = val1 * 2;
+                    } else {
+                        board[i][index++] = val1;
+                        stack.add(val2);
+                    }
+                } else {
+                    board[i][index++] = val1;
+                }
+            }
+
+
+        }
+    }
+
+    public static void right() {
+        for (int i = 0; i < N; i++) {
+            Stack<Integer> stack = new Stack<>();
+            for (int j = 0; j < N; j++) {
+                if (board[i][j] > 0) {
+                    stack.add(board[i][j]);
+                }
+                board[i][j] = 0;
+            }
+            int index = N - 1;
+
+            while (!stack.empty()) {
+                int val1 = stack.pop();
+                if (!stack.empty()) {
+                    int val2 = stack.pop();
+                    if (val1 == val2) {
+                        board[i][index--] = val1 * 2;
+                    } else {
+                        board[i][index--] = val1;
+                        stack.add(val2);
+                    }
+                } else {
+                    board[i][index--] = val1;
+                }
+            }
+
+        }
+    }
 }
