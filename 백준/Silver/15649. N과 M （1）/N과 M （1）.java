cba3496bb2d2a53fa0c370481f676969c96a97db
김성현 <60726663+kim-seonghyun@users.noreply.github.com>
2025-05-31 15:02:11 +0900

    [Silver III] Title: N과 M (1), Time: 604 ms, Memory: 42628 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,54 +1,40 @@
-/**
- * 자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
- * 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
- * 한 줄에 하나씩 문제의 조건을 만족하는 수열을 출력한다. 
- * 중복되는 수열을 여러 번 출력하면 안되며, 각 수열은 공백으로 구분해서 출력해야 한다.
- * 수열은 사전 순으로 증가하는 순서로 출력해야 한다.
- */
-
-
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-
-    //수열 저장 하는 배열
-    static int arr[];
-    //탐색 중 
-    static boolean visited[];
-    static StringBuilder sb = new StringBuilder();
-    
-    public static void main(String[] args) throws Exception{
-        // TODO Auto-generated method stub
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        
-        arr = new int[M];
-        visited = new boolean[N];
-        dfs(N, M, 0);
-        System.out.println(sb);
+    static int n;
+    static int m;
+    static BufferedReader br;
+    static StringTokenizer st;
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr =new int[m];
+        dfs(0, new boolean[n+1]);
     }
-    
-    public static void dfs(int N, int M, int depth) {
-        // 만약 탐색 길이가 M 동일하면 출력
-        if(depth == M) {
-            for(int num : arr) {
-                sb.append(num).append(" ");
+
+    private static void dfs(int depth, boolean[] visited) {
+        if(depth == m){
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i<m; i++){
+                sb.append(arr[i]).append(" ");
             }
-            sb.append("\n");
+            System.out.println(sb);
             return;
         }
-        
-        for(int i=0; i<N; i++) {
-            if(!visited[i]) {
+
+        for(int i =1; i<=n; i++){
+            if(!visited[i]){
                 visited[i] = true;
-                arr[depth] = i+1;
-                dfs(N, M, depth+1);
+                arr[depth] = i;
+                dfs(depth + 1, visited);
                 visited[i] = false;
             }
         }
     }
-
 }
